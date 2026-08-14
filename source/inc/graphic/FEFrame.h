bed130afa1c084d5381531a0eddb14f5325fc0b4
zhanglitong <13697826@qq.com>
2026-08-14 08:29:50 +0800

    new file

---
+++ source/inc/graphic/FEFrame.h
@@ -0,0 +1,53 @@
+#pragma     once
+
+#include    "RSObject.h"
+#include    "FEGImageView.h"
+#include    "FEFence.h"
+#include    "FESemaphore.h"
+#include    "FECmdBuffer.h"
+
+namespace   FE
+{
+    class   FEDevice;
+    using   Device  =   SharedPtr<FEDevice>;
+    
+    class   FEFrame :public FEObject
+    {
+    public:
+        friend  class   FECmdBuffer;
+        friend  class   FEQueue;
+        friend  class   VKQueue;
+        friend  class   VKSwapchain;
+        friend  class   FEScene;
+        friend  class   WGQueue;
+    public:
+        FEFrame(FEContext& ctx)
+            :FEObject(ctx)
+        {}
+        FEFrame(const FEFrame& other)
+            :FEObject(other)
+        {}
+
+        inline  void    reset()
+        {
+            if (_fenceWait)
+            {
+                _fenceWait->wait(UINT64_MAX);
+                _fenceWait->reset();
+            }
+        }
+    public:
+        CMDPtr      _cmd;
+    protected:
+        Fence       _fenceWait;
+        Semaphore   _semRenderComplete;
+        Semaphore   _semPresentComplete;
+        GImgView    _imageViewer;
+        uint32_t    _imageIdx   =   MaxUint32;
+        
+    };
+
+
+    using   Frame   =   SharedPtr<FEFrame>;
+    using   Frames  =   std::vector<Frame>;
+}
