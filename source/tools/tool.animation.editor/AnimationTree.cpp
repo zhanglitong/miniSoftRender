ae568df677ee86792d9b8946f1f459720e677881
zhanglitong <13697826@qq.com>
2026-09-15 23:10:49 +0800

    1

---
+++ source/tools/tool.animation.editor/AnimationTree.cpp
@@ -0,0 +1,345 @@
+#include    "AnimationTree.h"
+#include    <QHeaderView>
+#include    <QMouseEvent>
+#include    <QContextMenuEvent>
+#include    <QWheelEvent>
+#include    "UiTickMgr.h"
+#include    "AnimationItem.h"
+#include    "MainWindow.h"
+#include    "mesh/FEMesh.hpp"
+
+using    namespace    FE;
+
+inline  QIcon   iconOfObject(FEObject* object)
+{
+    if (object == nullptr)                      return  OBJECT_ICON(ID_LAYER);
+    else if (object->cast<FENode>())            return  OBJECT_ICON(ID_NODE);
+    else if (object->cast<FEAnimation>())       return  OBJECT_ICON(ID_ANIMATION);
+    else if (object->cast<FEKeyFrameTrack>())   return  OBJECT_ICON(ID_ATTRIBUTE);
+    else if (object->cast<FEMaterial>())        return  OBJECT_ICON(ID_MATERIAL);
+    else if (object->cast<FEMesh>())            return  OBJECT_ICON(ID_GEO);
+    else                                        return  OBJECT_ICON(ID_OBJECT);
+}
+inline  auto    nameOfObject(FEObject& item)
+{
+    auto    node    =   item.cast<FENode>();
+    if (node)
+        return  node->name();
+    else
+        return  item.className(); 
+}
+/// REGIST_SHORTCUT 在 UiTickMgr.cpp 中定义
+QShortcut*  REGIST_SHORTCUT(const std::string& str, QObject* parent);
+
+static  std::string   ObjText(FEObject* obj)
+{
+    assert(obj->className());
+    return  obj->className();
+}
+
+static  bool    IsExpand(FEObject& item)
+{
+    auto    node    =   item.cast<FENode>();
+    if (node)
+        return  node->flags().hasFlag(FE::FLAG_EXPAND);
+    else
+        return  item.flags().hasFlag(FE::FLAG_EXPAND);
+}
+
+AnimationTree::AnimationTree(QWidget* parent)
+    : QTreeView(parent)
+{
+    _model      =   new QStandardItemModel(this);
+    _model->setHorizontalHeaderLabels(QStringList()<<u8"动画");
+    auto    invisibleItem  =   _model->invisibleRootItem();
+    _rootItem   =   new AnimationItem(u8"总纲", AnimationItem::IT_Par, nullptr, iconOfObject(nullptr));
+    invisibleItem->appendRow(_rootItem);
+
+    this->setStyleSheet(R"(QTreeView::item {height: 40px;})");
+
+    setModel(_model);
+
+    connect(this,   SIGNAL(sigPaintEvent()),                _tickMgr,   SLOT(slotDoPaint()));
+    connect(this,   SIGNAL(expanded(const QModelIndex &)),  this,       SLOT(slotItemExpanded(const QModelIndex &)));
+    connect(this,   SIGNAL(collapsed(const QModelIndex &)), this,       SLOT(slotItemCollapsed(const QModelIndex &)));
+    connect(selectionModel(), SIGNAL(selectionChanged(const QItemSelection&, const QItemSelection&)), this, SLOT(slotSelectItemChanged(const QItemSelection&, const QItemSelection&)));
+
+    /// 添加右键菜单
+    _menu   =   new QMenu(this);
+    QAction*    deleteTrack = new QAction("删除轨道", this);
+    _menu->addAction(deleteTrack);
+    connect(deleteTrack, SIGNAL(triggered()), this, SLOT(slotDeleteTrack()));
+    _shortcutDelete     =   REGIST_SHORTCUT("Delete", this);
+    connect(_shortcutDelete, &QShortcut::activated, [&]()
+    {
+        auto    globaPos    =   QCursor::pos();
+        auto    localPos    =   mapFromGlobal(globaPos);
+
+        if (rect().contains(localPos))
+        {
+            slotDeleteTrack();
+        }
+    });
+}
+
+void    AnimationTree::linkToTickMgr(UiTickMgr* mgr)
+{
+    _tickMgr            =   mgr;
+    int keyRowHeight    =   visualRect(_rootItem->index()).height();
+    int timeRowHeight   =   header()->height();
+
+    _tickMgr->setKeyRowHeight(keyRowHeight);
+    _tickMgr->setTimeRowHeight(timeRowHeight);
+    _tickMgr->setAniTree(this);
+    connect(this, SIGNAL(sigPaintEvent()), _tickMgr, SLOT(slotDoPaint()));
+}
+
+void    AnimationTree::updateUi()
+{
+    _rootItem->removeRows(0, _rootItem->rowCount());
+    /// 更新树
+    AnimationItems  needExpand;
+    for (auto obj : _objects)
+    {
+        if (obj->cast<FENode>())
+        {
+            auto    pItem   =   createItemForNode(obj->cast<FENode>(),needExpand);
+            _rootItem->appendRow(pItem);
+        }
+        else
+        {
+            auto    pItem   =   createItemForObj(obj,needExpand);
+            _rootItem->appendRow(pItem);
+        }
+    }
+    for (auto& var : needExpand)
+    {
+        expand(_model->indexFromItem(var));
+    }
+}
+void    AnimationTree::selectObject(Object item,bool multiSelect)
+{
+    _objects    =   {item};
+    updateUi();
+}
+
+
+void    AnimationTree::slotItemExpanded(const QModelIndex& index)
+{
+    auto item = dynamic_cast<AnimationItem*>(_model->itemFromIndex(index));
+    item->setIsExpanded(true);
+}
+
+void    AnimationTree::slotItemCollapsed(const QModelIndex& index)
+{
+    auto item = dynamic_cast<AnimationItem*>(_model->itemFromIndex(index));
+    item->setIsExpanded(false);
+}
+
+void    AnimationTree::slotSelectItemChanged(const QItemSelection& cur, const QItemSelection& pre)
+{
+    _selectedItem = nullptr;
+    for (auto& index : cur.indexes())
+    {
+        auto item = dynamic_cast<AnimationItem*>(_model->itemFromIndex(index));
+        item->setIsSelected(true);
+
+        _selectedItem = item;
+    }
+
+    for (auto& index : pre.indexes())
+    {
+        auto item = dynamic_cast<AnimationItem*>(_model->itemFromIndex(index));
+        item->setIsSelected(false);
+    }
+}
+
+void    AnimationTree::slotDeleteTrack()
+{
+    if (!_selectedItem)
+    {
+        return;
+    }
+
+    /// 删除当前所有轨道
+    std::vector<FE::FEKeyFrameTrack*> tracks;
+    collectAllTrackItemChildren(_selectedItem, tracks);
+
+    if (tracks.empty())
+    {
+        return;
+    }
+
+    /// TODO: UNDO_STACK 待实现
+    /// UNDO_STACK->beginMacro(u8"删除轨道");
+    /// for (auto& pTrack : tracks)
+    /// {
+    ///     auto pAnim = pTrack->getAnimObject();
+    ///     UNDO_STACK->push(new RemoveKeyframeTrackCmd(pAnim, CUR_ANIMMIXER, CUR_ANIMCLIP, pTrack));
+    ///     if (pAnim->mixerGroupCnt() == 0)
+    ///     {
+    ///         UNDO_STACK->push(new SetAnimObjectCmd(pAnim->getObject(), nullptr));
+    ///     }
+    /// }
+    /// UNDO_STACK->endMacro();
+}
+
+void    AnimationTree::slotDoubleClikced(const QModelIndex& index)
+{
+    auto item = dynamic_cast<AnimationItem*>(_model->itemFromIndex(index));
+
+    while (item->parent() != _rootItem)
+    {
+        item = dynamic_cast<AnimationItem*>(item->parent());
+    }
+
+    auto    pObj    =   item->object();
+    auto    pNode   =   pObj ? pObj->cast<FENode>() : nullptr;
+    if (pNode)
+    {
+        if (_isSelectWhenMoveTo)
+        {
+            /// TODO: UNDO_STACK 待实现
+            /// UNDO_STACK->beginMacro(u8"动画树定位");
+            /// UNDO_STACK->push(new SelectCmd({ pNode }, true, SELECTEDOBJS, SELECT_FLAG_RECURSION));
+            /// UNDO_STACK->endMacro();
+        }
+
+        /// TODO: CELLSceneBrowser 待移植
+        /// CELLSceneBrowser& sceneBrowser = CELLSceneBrowser::Get(ENGINE->ctx());
+        /// auto    box =   pNode->globalAabb();
+        /// sceneBrowser.moveTo(box, 1.0, CELLSceneBrowser::AabbFaceIndex::None);
+    }
+}
+
+void    AnimationTree::mousePressEvent(QMouseEvent* evt)
+{
+    QModelIndex index = indexAt(evt->pos());
+    if (!index.isValid())
+    {
+        this->clearSelection();
+    }
+    QTreeView::mousePressEvent(evt);
+}
+
+void    AnimationTree::paintEvent(QPaintEvent* evt)
+{
+    QTreeView::paintEvent(evt);
+    /// 通知tickMgr更新
+    emit    sigPaintEvent();
+}
+
+void    AnimationTree::contextMenuEvent(QContextMenuEvent* event)
+{
+    if (!_selectedItem)
+        return;
+
+    _menu->exec(event->globalPos());
+
+    QWidget::contextMenuEvent(event);
+}
+
+void    AnimationTree::wheelEvent(QWheelEvent* event)
+{
+    auto    scrollBar   =   verticalScrollBar();
+    int     delta       =   event->angleDelta().y();
+    int     val         =   scrollBar->value();
+    if (delta > 0)
+        scrollBar->setValue(val - 1);
+    else if (delta < 0)
+        scrollBar->setValue(val + 1);
+}
+
+void    AnimationTree::mouseDoubleClickEvent(QMouseEvent* event)
+{
+    auto    pos     =   event->pos();
+    auto    index   =   indexAt(pos);
+    if (index.isValid() && _model->itemFromIndex(index) != _rootItem)
+    {
+        slotDoubleClikced(index);
+    }
+}
+
+AnimationItem*  AnimationTree::createItemForNode(FE::FENode* object, AnimationItems& needExpand)
+{
+    /// 建立树形结构
+    /// node
+    ///  - animation0
+    ///    - track0
+    ///    - track1
+    ///  - animation1
+    ///    - track0
+    ///    - track1
+    auto    rootItem    =   new AnimationItem(nameOfObject(*object), AnimationItem::IT_Par, object, iconOfObject(object));
+    /// 获取当前节点的所有动画数据
+    auto    anims       =   object->objects<FEAnimation>();
+    /// 遍历所有动画
+    for (auto anim : anims)
+    {
+        auto    animItem    =   new AnimationItem(anim->name().c_str(), AnimationItem::IT_Par, anim,iconOfObject(anim));
+        rootItem->appendRow(animItem);
+        auto    clip    =   anim->clip();
+        assert(clip != nullptr);
+        if (clip == nullptr)
+            continue;
+        auto    tracks  =   clip->tracks();
+        for (auto track : tracks)
+        {
+            animItem->appendRow(new AnimationItem(track->name().c_str(), AnimationItem::IT_Track, track,iconOfObject(track)));
+        }
+    }
+    return  rootItem;
+}
+
+AnimationItem*  AnimationTree::createItemForObj(FE::FEObject* pObject, AnimationItems& needExpand)
+{
+    /// 建立树形结构
+    /// Object(mat,geo...)
+    ///  - animation0
+    ///    - track0
+    ///    - track1
+    ///  - animation1
+    ///    - track0
+    ///    - track1
+    auto        rootItem    =   new AnimationItem(nameOfObject(*pObject), AnimationItem::IT_Par, pObject,iconOfObject(pObject));
+    Animations  anims;
+    pObject->traverseObject([&](const FEObject& object, const FEObject&, const FEObject::FETrvsCtx&, uint)->bool
+    {
+        auto    pAnim   =   dynamic_cast<const FEAnimation*>(&object);
+        if (pAnim)
+        {
+            Animation   tmp =   (FEAnimation*)pAnim;
+            anims.push_back(tmp);
+        }
+        return true;
+    });
+    /// 遍历所有动画
+    for (auto anim : anims)
+    {
+        auto    animItem    =   new AnimationItem(anim->name().c_str(), AnimationItem::IT_Par, anim,iconOfObject(anim));
+        rootItem->appendRow(animItem);
+        auto    clip    =   anim->clip();
+        assert(clip != nullptr);
+        if (clip == nullptr)
+            continue;
+        auto    tracks  =   clip->tracks();
+        for (auto track : tracks)
+        {
+            animItem->appendRow(new AnimationItem(track->name().c_str(), AnimationItem::IT_Track, track,iconOfObject(track)));
+        }
+    }
+    return  rootItem;
+}
+
+void    AnimationTree::collectAllTrackItemChildren(AnimationItem* item, std::vector<FE::FEKeyFrameTrack*>& results)
+{
+    auto pTrack = item->animKeyframeTrack();
+    if (pTrack)
+    {
+        results.push_back(pTrack);
+    }
+    for (int i = 0; i < item->rowCount(); ++i)
+    {
+        collectAllTrackItemChildren((AnimationItem*)item->child(i), results);
+    }
+}
---
+++ source/tools/tool.animation.editor/AnimationTree.h
@@ -0,0 +1,79 @@
+#pragma     once
+#include    <QTreeView>
+#include    <QShortcut>
+#include    <QStandardItemModel>
+#include    "node/FENode.hpp"
+#include    "animation/FEKeyFrameTrack.hpp"
+#include    "AnimationItem.h"
+
+class   UiTickMgr;
+class   AnimationItem;
+
+class   AnimationTree : public QTreeView
+{
+    Q_OBJECT
+public:
+    AnimationTree(QWidget* parent = nullptr);
+public:
+    void    linkToTickMgr(UiTickMgr* mgr);
+    void    updateUi();
+    void    setSelectWhenMoveTo(bool enable)
+    {
+        _isSelectWhenMoveTo = enable;
+    }
+    /// <summary>
+    /// 判断是否是展开状态
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    bool    isExpand(AnimationItem* item)
+    {
+        QModelIndex index = item->index();
+        if (index.isValid() && QTreeView::isExpanded(index)) 
+            return  true;
+        else
+            return  false;
+    }
+    /// <summary>
+    /// 获取根节点
+    /// </summary>
+    /// <returns></returns>
+    auto    rootItem() const
+    {
+        return  _rootItem;
+    }
+public:
+    /// <summary>
+    /// 模型树选择对象同通知,
+    /// </summary>
+    /// <param name="item">被选择对象</param>
+    /// <param name="multiSelect">是否是多选</param>
+    void    selectObject(Object item,bool multiSelect);
+signals:
+    void    sigPaintEvent();
+public slots:
+    void    slotItemExpanded(const QModelIndex & index);
+    void    slotItemCollapsed(const QModelIndex & index);
+    void    slotSelectItemChanged(const QItemSelection&, const QItemSelection&);
+    void    slotDeleteTrack();
+    void    slotDoubleClikced(const QModelIndex&);
+public:
+    virtual void    mousePressEvent(QMouseEvent* evt)           override;
+    virtual void    paintEvent(QPaintEvent* evt)                override;
+    virtual void    contextMenuEvent(QContextMenuEvent* event)  override;
+    virtual void    wheelEvent(QWheelEvent* event)              override;
+    virtual void    mouseDoubleClickEvent(QMouseEvent *event)   override ;
+private:
+    AnimationItem*  createItemForNode(FE::FENode* object,  AnimationItems& needExpand);
+    AnimationItem*  createItemForObj(FE::FEObject* object, AnimationItems& needExpand);
+    void            collectAllTrackItemChildren(AnimationItem* item, std::vector<FE::FEKeyFrameTrack*>& results);
+private:
+    FE::Objects             _objects;
+    QStandardItemModel*     _model              =   nullptr;
+    QMenu*                  _menu               =   nullptr;
+    UiTickMgr*              _tickMgr            =   nullptr;
+    AnimationItem*          _rootItem           =   nullptr;
+    AnimationItem*          _selectedItem       =   nullptr;
+    QShortcut*              _shortcutDelete     =   nullptr;
+    bool                    _isSelectWhenMoveTo =   false;
+};
